27837077c8e5e7854edd22a2ffdc6bdf25e8519e
Josh8396 <joshuaa8396@gmail.com>
2025-06-30 23:32:22 -0500

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-
-// Simulación de login con Google (Firebase Auth se integra en producción)
-document.getElementById('loginBtn').addEventListener('click', function() {
-  alert('Simulando inicio de sesión...');
-  document.getElementById('reportForm').style.display = 'block';
-});
