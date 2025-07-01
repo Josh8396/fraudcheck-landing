aa8cefb3417a4fed707865a9c7d8e749f9179e62
Josh8396 <joshuaa8396@gmail.com>
2025-06-30 23:34:58 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+
+document.getElementById('loginBtn').addEventListener('click', function() {
+  alert('Simulando inicio de sesión...');
+  document.getElementById('reportForm').style.display = 'block';
+});
